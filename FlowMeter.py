5df5ab95df6b41ebcff8489363257800b0919f3d
Jerome Fischer <jerome.fischer.02@outlook.com>
2021-05-21 11:08:29 +0200

    changed flow meter class

--- FlowMeter.py
+++ FlowMeter.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+import datetime
 
 from Logger import logger
 
@@ -12,25 +13,25 @@ count = 0
 class FlowMeter:
 
     def __init__(self, pin, name):
+
         self.pin = pin
         self.name = name
-
         self.init_gpio()
 
     def init_gpio(self):
         logger.info('initialize: {} pin: {}'.format(self.name, self.pin))
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.count_pulse)
 
     def count_pulse(self, channel):
         global count
-        count = count + 1
-
+        count = count+1
+        
     def get_flow_rate(self):
         global count
-        print(count)
         flow = count / 6  # pulse frequency = 6*Q, Q is the flow rate in L/min
         logger.info("Flow {}: {} Liter/min".format(self.name, flow))
+        print("Flow {}: {} Liter/min".format(self.name, flow))
         count = 0
-
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logging.handlers import RotatingFileHandler
 import RPi.GPIO as GPIO
 import time
 import logging
+import threading
 
 from FlowMeter import FlowMeter
 from Logger import logger
@@ -14,31 +15,34 @@ from Valve import Valve
 
 GPIO_VALVE = 23
 
-start_counter = 0
+
+def flow_threading():
+    while True:
+        f1.get_flow_rate()
+        time.sleep(1)
+    
 
 if __name__ == '__main__':
-
+    
     logger.info('started...')
 
     v1 = Valve(pin=GPIO_VALVE)
-    v1.set_valve_on()
-    time.sleep(1)
-    v1.set_valve_off()
-
+     
+# 
     t1 = TemperatureSensor(location='water', sensor_type='DS18B20', sensor_name='/sys/bus/w1/devices/28-01131a6b6e1c/w1_slave')
     temperature = t1.read_temp()['sensor_measurement']
     logger.info('{} measured temperature: {} °C'.format(t1.location, temperature))
 
     f1 = FlowMeter(pin=5, name='circulating')
-    GPIO.add_event_detect(f1.pin, GPIO.FALLING, callback=f1.count_pulse)
-
-
+    x = threading.Thread(target=flow_threading)
+    x.start()
 
     while True:
         try:
-            f1.get_flow_rate()
+            v1.set_valve_on()
             time.sleep(1)
-
+            v1.set_valve_off()
+            time.sleep(1)
         except KeyboardInterrupt:
             GPIO.cleanup()
 
